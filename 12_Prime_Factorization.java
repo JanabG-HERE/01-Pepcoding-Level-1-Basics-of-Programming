912a747d283988dd78ce618032a0591b5ba47959
Aman Pratap <amanpratap1026@gmail.com>
2021-09-07 19:26:56 +0530

    Create 12_Prime_Factorization.java

---
+++ 12_Prime_Factorization.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class Main{
+
+public static void main(String[] args) {
+  // write your code here
+  Scanner sc = new Scanner(System.in);
+  int n = sc.nextInt();
+  for(int i=2;i<=Math.sqrt(n);i++)
+  {
+      while(n%i==0)
+      {
+          System.out.print(i+" ");
+          n=n/i;
+      }
+  }
+  if(n>1)
+  {
+      System.out.print(n);
+  }
+ }
+}
+//https://www.pepcoding.com/resources/online-java-foundation/getting-started/prime-factorisation-official/ojquestion
